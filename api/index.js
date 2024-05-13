45c0278f03178c7c3296fd68e8cfb9984e85b7bc
rithvik reddy <147538307+rithvikreddybasani@users.noreply.github.com>
2024-05-13 11:57:56 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin : "http://localhost:3000",
+    origin : "https://mern-chat-app-client-mauve.vercel.app",
     credentials : true
 }))
 app.use(express.json())
